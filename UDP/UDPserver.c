33ec945023670fec4046c6c868d7fb57dcafce17
Orsrrc <2235694867@qq.com>
2024-01-24 20:18:21 +0800

    update

--- UDP/UDPserver.c
+++ UDP/UDPserver.c
@@ -4,16 +4,23 @@
 int main(void)
 {
     //创建套接字
-    printf("创建套接字");
-    int SocketID = socket(PF_INET, SOCK_DGRAM, 0 );
-    if(SocketID == -1)
+    printf("创建套接字\n");
+    int socketfd = socket(PF_INET, SOCK_DGRAM, 0 ); //fd == file descriptor
+    if(socketfd == -1) 
     {
         perror("socket");
         return -1;
     }
 
-    //准备地质结构
+    //准备地址结构
+    struct sockaddr server;
+    server.sa_family = AF_INET;
 
-
+    //绑定套接字
+    if(bind(socketfd, &server, (socklen_t)server.sa_len) == -1)
+    {
+        perror("bind");
+        return -1;
+    }
     return 0;
 }
